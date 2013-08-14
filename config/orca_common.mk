164217f00d406e9e1b49527f2daa2044b68e4b38
Orca Project <shamukillaz@orcaproject.co>
2013-08-13 23:48:04 -0400

    Fix PA Prebuilts

--- config/orca_common.mk
+++ config/orca_common.mk
@@ -93,8 +93,8 @@ else
 endif
 
 PRODUCT_COPY_FILES += \
-    vendor/pa/prebuilt/$(PA_CONF_SOURCE).conf:system/etc/paranoid/properties.conf \
-    vendor/pa/prebuilt/$(PA_CONF_SOURCE).conf:system/etc/paranoid/backup.conf
+    vendor/orca/prebuilt/$(PA_CONF_SOURCE).conf:system/etc/paranoid/properties.conf \
+    vendor/orca/prebuilt/$(PA_CONF_SOURCE).conf:system/etc/paranoid/backup.conf
 
 ORCA_VERSION_MAJOR = 3
 ORCA_VERSION_MINOR = 0
